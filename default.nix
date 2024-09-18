24b98ed67a0453c8f21572c44cb8e2839a312188
henrispriet <henri.spriet@gmail.com>
2024-09-19 01:15:32 +0200

    add attr to test one example

--- default.nix
+++ default.nix
@@ -7,12 +7,13 @@ let
   inherit (pkgs.lib) path;
 
   mkExample = pkgs.callPackage ./mk-example.nix {};
+  mkTest = pkgs.callPackage (import ./mk-test.nix) {};
+
   examples = mapAttrs
     (pname: _: mkExample { inherit pname; src = (path.append examplesDir pname); })
     (filterAttrs (n: v: v == "directory") (builtins.readDir examplesDir));
   exampleDrvs = attrValues examples;
 
-  mkTest = pkgs.callPackage (import ./mk-test.nix) {};
 in {
   # build all examples
   all = pkgs.buildEnv {
@@ -26,6 +27,11 @@ in {
     paths = (builtins.map (mkTest "wayland") exampleDrvs)
          ++ (builtins.map (mkTest "x11") exampleDrvs);
   };
+
+  # test one example
+  # example use: `nix-build -A test --argstr displayServer wayland --argstr example arc`
+  # example use: `nix-build -A test.driverInteractive --argstr displayServer wayland --argstr example arc`
+  test = { displayServer, example }: mkTest displayServer examples.${example};
 
   shell = pkgs.mkShell {
     inputsFrom = exampleDrvs;
